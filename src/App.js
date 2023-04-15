92bb09ededf1750e835c48502d248ef0547cf3bf
MohitGupta-16 <mohitgupta.mg2727@gmail.com>
2023-04-15 23:23:31 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SignUp from "./Components/SignUp"
+import "./App.css"
+
+function App() {
+    return (
+        <div>
+            <SignUp></SignUp>
+        </div>
+    )
+}
+
+export default App
